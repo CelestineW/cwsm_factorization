9012355508acbda93b25b83988b62368f80432a6
Sara Masoudi <sm14@umbc.edu>
2017-04-24 19:28:08 -0400

    started pollard rho

--- Factorization/src/Factor.java
+++ Factorization/src/Factor.java
@@ -1,6 +1,21 @@
-
+import java.math.*;
+import java.util.*;
 
 public class Factor {
+	
+	public static void PollardRho(BigInteger n) {
+		
+		int i = 1;
+		Random rand = new Random();
+		BigInteger x1 = new BigInteger(n.bitLength(), rand);
+		BigInteger y = x1;
+		int k = 2;
+		
+		while (true) {
+		
+			i = i + 1; 			
+		}
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -9,5 +24,5 @@ public class Factor {
 		System.out.println("Sara trying to commit");
 		System.out.println("Sara trying to commit again");
 	}
-
+	
 }
