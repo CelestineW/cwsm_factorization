fea7a4cb0572f96d13e0bcdfa2a14c421ab21cc0
Sara Masoudi <sm14@umbc.edu>
2017-04-24 18:58:17 -0400

    second commit

--- Factorization/src/Factor.java
+++ Factorization/src/Factor.java
@@ -1,11 +1,13 @@
 
-public class Factor {
 
+public class Factor {
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Factorizing Moduli");
 		System.out.println("trying again to commit/print");
 		System.out.println("Sara trying to commit");
+		System.out.println("Sara trying to commit again");
 	}
 
 }
