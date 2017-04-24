211d5927e4eaa22efdcabdb08abb3250000d7dd3
cwong2 <cwong2@umbc.edu>
2017-04-24 18:26:45 -0400

    Another commit by Celeste

--- Factorization/src/Factor.java
+++ Factorization/src/Factor.java
@@ -4,6 +4,7 @@ public class Factor {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Factorizing Moduli");
+		System.out.println("trying again to commit/print");
 
 	}
 
