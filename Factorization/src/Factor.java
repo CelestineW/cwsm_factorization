01ca505e33133daa62663dec0a785cb85a939a35
cwong2 <cwong2@umbc.edu>
2017-04-24 20:02:41 -0400

    wrote getRandBigInt function, to use for generating random BigIntegers,
    pass in size of n

--- Factorization/src/Factor.java
+++ Factorization/src/Factor.java
@@ -3,6 +3,8 @@ import java.util.*;
 
 public class Factor {
 	
+	static Random rand = new Random();
+	
 	public static void PollardRho(BigInteger n) {
 		
 		int i = 1;
@@ -17,12 +19,50 @@ public class Factor {
 		}
 	}
 	
+	public static void PollardRhoMinusOneBootstrap(BigInteger n){
+		
+		BigInteger a = getRandBigInt(n); // getting a base a,
+		
+		
+		
+		
+	}
+	
+	public static boolean PollardRhoMinusOne(BigInteger n, BigInteger a){
+		
+		a = getRandBigInt(n);
+		
+		System.out.println(a);
+		
+		return true;
+		
+	}
+	
+	public static BigInteger getRandBigInt(BigInteger size) {
+		
+	  
+	    BigInteger randBigInt = new BigInteger(size.bitLength(), rand);
+	    
+	    while( randBigInt.compareTo(size) >= 0 ) {
+	    	
+	    	// Regenerate BigInteger
+	        randBigInt = new BigInteger(size.bitLength(), rand);
+	    }
+	    return randBigInt;
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Factorizing Moduli");
 		System.out.println("trying again to commit/print");
 		System.out.println("Sara trying to commit");
 		System.out.println("Sara trying to commit again");
+		
+		BigInteger a = getRandBigInt(BigInteger.valueOf(1024));
+		
+		System.out.println(a);
+		
 	}
 	
 }
