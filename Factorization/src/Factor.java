29595952297c61038cf547db37075e05d193ecab
cwong2 <cwong2@umbc.edu>
2017-04-24 22:12:23 -0400

    Pollard P-1 Method, returns a factor of n

--- Factorization/src/Factor.java
+++ Factorization/src/Factor.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Factor {
 	
 	static Random rand = new Random();
+
 	
 	public static void PollardRho(BigInteger n) {
 		
@@ -19,28 +20,52 @@ public class Factor {
 		}
 	}
 	
-	public static void PollardRhoMinusOneBootstrap(BigInteger n){
+	public static BigInteger PollardPMinusOne(BigInteger n){
 		
 		BigInteger a = getRandBigInt(n); // getting a base a,
+		a = BigInteger.valueOf(2); // For Testing
 		
 		
+		System.out.println("Your num: " + n);
+		
+		System.out.println("Your base, a : " + a);
 		
 		
-	}
-	
-	public static boolean PollardRhoMinusOne(BigInteger n, BigInteger a){
+		BigInteger m = a; // gets accumulated for faster computations
 		
-		a = getRandBigInt(n);
 		
-		System.out.println(a);
+		BigInteger bound = new BigInteger("1000000");
 		
-		return true;
-		
+		for (BigInteger i = BigInteger.valueOf(2); i.compareTo(bound) <  0 ; i = i.add(BigInteger.ONE)){	
+			
+			// (a ^ k! mod n)
+			m = m.modPow(i, n);
+			
+			System.out.println("Your m, what you are gcding " + m.subtract(BigInteger.ONE));
+			
+			BigInteger gcdRes = n.gcd(m.subtract(BigInteger.ONE));
+			
+			System.out.println("GCD Result: " + gcdRes);
+			
+			
+			// Non-Trivial
+			if (gcdRes.compareTo(BigInteger.ONE) != 0){
+				
+				System.out.println("Returning!!!" + gcdRes);	
+				return gcdRes;
+			}
+			
+			System.out.println("Iterations: " + i.subtract(BigInteger.ONE));
+			
+		}
+				
+		return BigInteger.valueOf(-1);
 	}
 	
 	public static BigInteger getRandBigInt(BigInteger size) {
 		
-	  
+		//System.out.println(size.bitLength());
+		
 	    BigInteger randBigInt = new BigInteger(size.bitLength(), rand);
 	    
 	    while( randBigInt.compareTo(size) >= 0 ) {
@@ -59,10 +84,17 @@ public class Factor {
 		System.out.println("Sara trying to commit");
 		System.out.println("Sara trying to commit again");
 		
+		// Sample n 
 		BigInteger a = getRandBigInt(BigInteger.valueOf(1024));
 		
-		System.out.println(a);
+		BigInteger j = new BigInteger("5874944399468278831");
 		
+		BigInteger k = new BigInteger("1633339503459119690580928718149821598898458545541617999969136559642034201952904677527967822044015401967438017");
+		BigInteger i = new BigInteger("15581958524003781659");
+
+		
+		
+		PollardPMinusOne(i);
 	}
 	
 }
